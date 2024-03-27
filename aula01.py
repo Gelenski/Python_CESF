05ab6d5e2829a3fd06c4ab1173efbe5629441cee
Lucas Gelenski <gelenskilucas@gmail.com>
2024-03-27 10:33:46 -0300

    Usando prints

--- aula01.py
+++ aula01.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import os
+# print("Hello World!")
+print("O 3DS é a turma número 1 do Sagrada.")
+turma = "2DS"
+pos = 2
+print("O", turma, "é a turma número", pos, "do Sagrada.")
+print(type(turma))
+print(type(pos))
